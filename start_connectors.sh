b6f36329c5858826e4d7b76778c122b180e33379
nachemic <nachemic@gmail.com>
2026-07-02 23:42:18 +0200

    Add topic pre-creation to start_connectors.sh and update sink connector configuration

--- start_connectors.sh
+++ start_connectors.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Creando topics"
+docker exec broker-1 kafka-topics --bootstrap-server broker-1:29092 --create --if-not-exists --topic sensor-telemetry --partitions 1 --replication-factor 3
+docker exec broker-1 kafka-topics --bootstrap-server broker-1:29092 --create --if-not-exists --topic sensor-alerts --partitions 1 --replication-factor 3
+docker exec broker-1 kafka-topics --bootstrap-server broker-1:29092 --create --if-not-exists --topic sales-transactions --partitions 1 --replication-factor 3
+docker exec broker-1 kafka-topics --bootstrap-server broker-1:29092 --create --if-not-exists --topic sales-summary --partitions 1 --replication-factor 3
+
 echo "Lanzando conectores"
 
 curl -s -d @"./connectors/source-datagen-_transactions.json" -H "Content-Type: application/json" -X POST http://localhost:8083/connectors
